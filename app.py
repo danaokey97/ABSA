ee7faeb2908cc7d52e2f7b0fb3fea9ad048ec0ed
danaokey97 <111734342+danaokey97@users.noreply.github.com>
2026-01-07 22:55:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1087,7 +1087,7 @@ def run_absa_on_dataframe(df_raw, _sent_models):
 
             # Dataset: preprocessing sentimen juga tetap "ringan" (tanpa lexicon)
             sent_label, _ = predict_sentiment_for_segment(
-                seg_text, aspek, _sent_models, use_lexicon=True
+                seg_text, aspek, _sent_models, use_lexicon=False
             )
 
             data_rows.append({
